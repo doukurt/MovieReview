d5f1bafea31f91ab1415db4a30e95487f2b08dde
doukurt <d.kurt_07@hotmail.com>
2021-03-17 17:10:33 +0300

    changes

--- MRVue/movie/src/Services/MovieService.js
+++ MRVue/movie/src/Services/MovieService.js
@@ -8,17 +8,17 @@ export default {
     }
 ,
 getSimilarMovies(id){
-    return axios.get(`https://api.themoviedb.org/3/movie/${id}/similar?api_key=63c20ab0688ef19ee1cdf0eacf135738&language=en-US`)
+    return axios.get(`https://api.themoviedb.org/3/movie/${id}/similar?api_key=YOUR_API_KEY&language=en-US`)
     },
     getMovieCredits(id){
-      return axios.get(`https://api.themoviedb.org/3/movie/${id}/credits?api_key=63c20ab0688ef19ee1cdf0eacf135738&language=en-US`)
+      return axios.get(`https://api.themoviedb.org/3/movie/${id}/credits?api_key=YOUR_API_KEY&language=en-US`)
     },
     getMovieDetails(id){
-      return axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=63c20ab0688ef19ee1cdf0eacf135738&language=en-US`)
+      return axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=YOUR_API_KEY&language=en-US`)
     
     },
     getMovieVideos(id){
-      return axios.get(`https://api.themoviedb.org/3/movie/${id}/videos?api_key=63c20ab0688ef19ee1cdf0eacf135738&language=en-US`)
+      return axios.get(`https://api.themoviedb.org/3/movie/${id}/videos?api_key=YOUR_API_KEY&language=en-US`)
     },
     getCommentsOfMovie(id){
       return axios.get(`http://localhost:3000/movie/${id}`)
